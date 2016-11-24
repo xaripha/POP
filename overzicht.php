b87d8d4d1022c8e8136d5a4500c5f28b4b547325
lynnaurelie <lynnaurelie@hotmail.com>
2016-11-24 11:25:32 +0100

    Update overzicht.php

--- overzicht.php
+++ overzicht.php
@@ -1,9 +1,14 @@
 <?php
 
+    include "conect.php";
+	$mysql = mysqli_connect($server,$user,$pass,$db) or die("Fout: Er is geen verbinding met de MySQL-server tot stand gebracht!");
+	
+	$resultaat = mysqli_query($mysql,"SELECT * FROM gebruikers WHERE mentor={$_SESSION['gebruikersnaam2']} ORDER BY achternaam") or die("De selectquery op de database is mislukt!");
+		
+	mysqli_close($mysql) or die("Het verbreken van de verbinding met de MySQL-server is mislukt!"); 
 
-
-
-
-
-
-?>
+	while(list($voornaam,$tussenvoegsel,$achternaam) = mysqli_fetch_row($resultaat))   
+{     
+ echo"$voornaam,$achternaam <br />";   
+}
+	
