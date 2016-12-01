d3110111d729cc831c5adc5fe18b261dc6eeba91
lynnaurelie <lynnaurelie@hotmail.com>
2016-12-01 11:49:23 +0100

    Update mentorgroep.php

--- mentorgroep.php
+++ mentorgroep.php
@@ -1,14 +1,18 @@
 <?php
-
     include "conect.php";
-	$mysql = mysqli_connect($server,$user,$pass,$db) or die("Fout: Er is geen verbinding met de MySQL-server tot stand gebracht!");
+	$mysql = mysqli_connect($server,$user,$pass,$db) 
+	  or die("Fout: Er is geen verbinding met de MySQL-server tot stand gebracht!");
 	
-	$resultaat = mysqli_query($mysql,"SELECT * FROM gebruikers WHERE mentor={$_SESSION['gebruikersnaam2']} ORDER BY achternaam") or die("De selectquery op de database is mislukt!");
+	$resultaat = mysqli_query($mysql,"SELECT * FROM gebruikers WHERE mentor={$_SESSION['gebruikersnaam2']} ORDER BY achternaam") 
+	  or die("De selectquery op de database is mislukt!");
 		
-	mysqli_close($mysql) or die("Het verbreken van de verbinding met de MySQL-server is mislukt!"); 
-
-	while(list($gebruikers_id,$voornaam,$tussenvoegsel,$achternaam) = mysqli_fetch_row($resultaat))   
+	mysqli_close($mysql) 
+	  or die("Het verbreken van de verbinding met de MySQL-server is mislukt!"); 
+	  
+	while(list($gebruiker_id,$voornaam,$tussenvoegsel,$achternaam) = mysqli_fetch_row($resultaat))   
 {     
- echo"$gebruikers_id,$voornaam,$tussenvoegsel,$achternaam <br />";   
+ echo"$gebruiker_id,$voornaam,$tussenvoegsel,$achternaam <br />";   
 } 
 	
+?>
+<a href="popoverzicht.php?gebruiker_id=$gebruiker_id"></a>
