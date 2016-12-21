88a36f8c21ef657f60a894d6ef2270742cee984c
lynnaurelie <lynnaurelie@hotmail.com>
2016-12-21 12:47:07 +0100

    Update popoverzicht.php

--- popoverzicht.php
+++ popoverzicht.php
@@ -1,11 +1,11 @@
 <?php
-    include "conect.php";
+    include "connect.php";
 	
 	$mysql = mysqli_connect($server,$user,$pass,$db) 
 	  or die("Fout: Er is geen verbinding met de MySQL-server tot stand gebracht!");
 if(isset(	$_GET['gebruiker_id'] )){
-	$resultaat = mysqli_query($mysql,"SELECT voornaam, tussenvoegsel, achternaam, gebruiker_id, datum
-                                      FROM gebruiker g, invoer i 
+	$resultaat = mysqli_query($mysql,"SELECT voornaam, tussenvoegsel, achternaam, g.gebruiker_id, datum
+                                      FROM gebruikers g, invoer i 
 									  WHERE i.gebruiker_id = {$_GET['gebruiker_id']} 
 									  ORDER BY achternaam") 
 	  or die("De selectquery op de database is mislukt!");
@@ -15,7 +15,7 @@ if(isset(	$_GET['gebruiker_id'] )){
 	  
 	while($pop = mysqli_fetch_assoc($resultaat))   
 {     
- echo"{$pop['gebruiker_id']} {$pop['datum']} {$pop['voornaam']} {$pop['tussenvoegsel']} {$pop['achternaam']} <br />";   
+ echo"<a href='popoverzicht.php?gebruiker_id={$pop['gebruiker_id']}'>{$pop['datum']} {$pop['voornaam']} {$pop['tussenvoegsel']} {$pop['achternaam']}<a/><br />";   
 } 
 }
 
