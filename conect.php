97f3c3b353ef8d23918635b7449f9979b5d9196b
denisegultekin <nuray140@hotmail.com>
2016-12-21 17:28:29 +0100

    Delete conect.php

--- conect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$server="localhost"; //je eigen pc
-$user="root";
-$pass="usbw";
-$db="pop"; //database waar we nu mee gaan experimenteren
-$mysql = mysqli_connect($server,$user,$pass,$db) or die("Fout: Er is geen verbinding met de MySQL-server tot stand gebracht!".mysqli_connect_error());
-?>
