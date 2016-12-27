6a737781f29c91f4bee740b13b683f07c24c45d6
lynnaurelie <lynnaurelie@hotmail.com>
2016-12-27 16:11:33 +0100

    Update home.php

--- home.php
+++ home.php
@@ -1,21 +1,23 @@
 <?php
-include "conect.php";
+include "connect.php";
 //var_dump($_SESSION); om te checken of alle sessions gevuld zijn
 
 ?>
 
 <html>
 <head> 
-<link rel="stylesheet" type="text/css" href="popmaak2.css">
 <title>home</title>
+<img id="imgpop" src="pop.jpg" alt="POP" style="width:150px;height:120px;"> <br/>
+<link rel="stylesheet" type="text/css" href="popmaak2.css">
  </head>
 
  <body>
+<table id="t8">
 <?php
-include "navigationbar.php";
 $voornaam=$_SESSION["voornaam"];
 $achternaam=$_SESSION["achternaam"];
-echo" welkom ".$voornaam." ".$achternaam." bij POP";
+echo"<div id='welkom'> Fijn dat je bent ingelogd! Welkom bij POP, ".$voornaam." ".$achternaam.".</div>";
+include "navigationbar.php";
 
 ?>
 
